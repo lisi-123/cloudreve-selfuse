ba674f7c86a81de5faf45cbbb3e3123efb9f581a
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2025-03-18 01:45:05 +0800

    Create cloudreve-running.sh

---
+++ cloudreve-running.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y screen
+
+sudo apt install cron -y
+
+chmod +x /root/cloudreve-selfuse/restart_wangpan.sh
+
+(crontab -l 2>/dev/null; echo "*/10 * * * * /root/cloudreve-selfuse/restart_wangpan.sh"; echo "0 0 */3 * * rm -f /root/cloudreve/restart_wangpan.log") | sort -u | crontab -
+
+screen -dmS wangpan_session /root/cloudreve-selfuse/cloudreve
