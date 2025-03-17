85baa0c04f59be93d91ecfd7434684cd15fa50ce
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2025-03-17 23:00:17 +0800

    Create cloudreves-start.sh

---
+++ cloudreves-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 安装压缩程序
+apt update && apt install unzip -y
+
+# 解压缩文件
+unzip /root/cloudreve-selfuse/cloudreve-amd64.zip -d /root/cloudreve-selfuse/
+
+# 赋予执行权限
+chmod +x /root/cloudreve-selfuse/cloudreve
+
+# 启动cloudreve
+sudo /root/cloudreve-selfuse/cloudreve.sh
